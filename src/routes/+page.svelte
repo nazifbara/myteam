cbc11e928817f58134a7f14923c9d6ce27f3a046
nazifbara <nazifbarassounon@gmail.com>
2023-12-07 17:54:35 +0100

    add classes for headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<div class="wrapper">
-	<h1 class="text-5xl">myteam</h1>
+<div class="wrapper grid gap-6">
+	<div class="grid gap-4">
+		<h1 class="text-5xl mb-3 text-primary">Typography</h1>
 
-	<p>
-		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse aspernatur dolore quas
-		necessitatibus itaque corporis, iure quam hic nam eaque rem repellendus, modi natus blanditiis
-		velit eligendi quidem voluptatibus? Cumque alias optio consectetur voluptas blanditiis sequi?
-		Corrupti adipisci veritatis unde facere ea saepe accusamus alias quia et est assumenda,
-		distinctio architecto minima quas deserunt dolorem officia voluptatem tempore nihil, facilis
-		dolore aut voluptates! Optio, corporis autem ad praesentium fugiat distinctio asperiores
-		voluptate nisi in atque non, numquam necessitatibus ut at ratione quasi, sit reprehenderit
-		temporibus incidunt? Obcaecati tenetur iste suscipit similique repellendus libero animi ducimus?
-		Suscipit omnis possimus quibusdam tempora.
-	</p>
+		<h1 class="heading heading--1">Quick waltz</h1>
+		<h2 class="heading heading--2">Quick waltz</h2>
+		<h3 class="heading">Quick waltz</h3>
+	</div>
 </div>
